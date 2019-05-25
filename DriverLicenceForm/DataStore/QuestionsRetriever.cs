6a3f61247f9615eb4bb3413531ea24d7f4457f1a
tucaliucmariana <tucaliuc.mariana@gmail.com>
2019-05-25 14:13:39 +0300

    DataStore class

---
+++ DriverLicenceForm/DataStore/QuestionsRetriever.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStore
+{
+    public class QuestionsRetriever
+    {
+        private List<Question> _questions;
+        public List<Question> QuestionsList {
+            get
+            {
+                return _questions;
+            }
+        }
+        QuestionsRetriever()
+        {
+            _questions = new List<Question>()
+            {
+                new Question(),
+                new Question()
+            };
+        }
+
+    }
+}
