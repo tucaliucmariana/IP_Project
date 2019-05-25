23a8c5075b4700857ce3cab326caa684b283d8db
tucaliucmariana <tucaliuc.mariana@gmail.com>
2019-05-25 17:15:15 +0300

    Calculate result of the quiz

--- DriverLicenceForm/DriverLicenceForm/FormQuiz.Designer.cs
+++ DriverLicenceForm/DriverLicenceForm/FormQuiz.Designer.cs
@@ -33,14 +33,12 @@
             this.buttonSubmit = new System.Windows.Forms.Button();
             this.labelQuestions = new System.Windows.Forms.Label();
             this.groupBoxAnswers = new System.Windows.Forms.GroupBox();
-            this.labelD = new System.Windows.Forms.Label();
             this.labelC = new System.Windows.Forms.Label();
             this.labelB = new System.Windows.Forms.Label();
             this.labelA = new System.Windows.Forms.Label();
-            this.radioButtonA = new System.Windows.Forms.RadioButton();
-            this.radioButtonD = new System.Windows.Forms.RadioButton();
-            this.radioButtonC = new System.Windows.Forms.RadioButton();
-            this.radioButtonB = new System.Windows.Forms.RadioButton();
+            this.A = new System.Windows.Forms.RadioButton();
+            this.C = new System.Windows.Forms.RadioButton();
+            this.B = new System.Windows.Forms.RadioButton();
             this.panelIntro = new System.Windows.Forms.Panel();
             this.textBoxTitle = new System.Windows.Forms.TextBox();
             this.buttonStart = new System.Windows.Forms.Button();
@@ -55,147 +53,142 @@
             this.panelQuestion.Controls.Add(this.buttonSubmit);
             this.panelQuestion.Controls.Add(this.labelQuestions);
             this.panelQuestion.Controls.Add(this.groupBoxAnswers);
-            this.panelQuestion.Location = new System.Drawing.Point(14, 23);
+            this.panelQuestion.Location = new System.Drawing.Point(19, 28);
+            this.panelQuestion.Margin = new System.Windows.Forms.Padding(4);
             this.panelQuestion.Name = "panelQuestion";
-            this.panelQuestion.Size = new System.Drawing.Size(662, 432);
+            this.panelQuestion.Size = new System.Drawing.Size(883, 532);
             this.panelQuestion.TabIndex = 4;
             // 
             // textBoxQuestion
             // 
-            this.textBoxQuestion.Location = new System.Drawing.Point(29, 48);
+            this.textBoxQuestion.Location = new System.Drawing.Point(39, 59);
+            this.textBoxQuestion.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxQuestion.Multiline = true;
             this.textBoxQuestion.Name = "textBoxQuestion";
-            this.textBoxQuestion.Size = new System.Drawing.Size(586, 140);
+            this.textBoxQuestion.Size = new System.Drawing.Size(780, 171);
             this.textBoxQuestion.TabIndex = 4;
             // 
             // buttonSubmit
             // 
-            this.buttonSubmit.Location = new System.Drawing.Point(550, 382);
+            this.buttonSubmit.Location = new System.Drawing.Point(733, 470);
+            this.buttonSubmit.Margin = new System.Windows.Forms.Padding(4);
             this.buttonSubmit.Name = "buttonSubmit";
-            this.buttonSubmit.Size = new System.Drawing.Size(75, 29);
+            this.buttonSubmit.Size = new System.Drawing.Size(100, 36);
             this.buttonSubmit.TabIndex = 3;
             this.buttonSubmit.Text = "Trimite";
             this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.ButtonSubmit_Click);
             // 
             // labelQuestions
             // 
             this.labelQuestions.AutoSize = true;
-            this.labelQuestions.Location = new System.Drawing.Point(36, 15);
+            this.labelQuestions.Location = new System.Drawing.Point(48, 18);
+            this.labelQuestions.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelQuestions.Name = "labelQuestions";
-            this.labelQuestions.Size = new System.Drawing.Size(52, 13);
+            this.labelQuestions.Size = new System.Drawing.Size(69, 17);
             this.labelQuestions.TabIndex = 2;
             this.labelQuestions.Text = "Întrebare:";
             // 
             // groupBoxAnswers
             // 
-            this.groupBoxAnswers.Controls.Add(this.labelD);
             this.groupBoxAnswers.Controls.Add(this.labelC);
             this.groupBoxAnswers.Controls.Add(this.labelB);
             this.groupBoxAnswers.Controls.Add(this.labelA);
-            this.groupBoxAnswers.Controls.Add(this.radioButtonA);
-            this.groupBoxAnswers.Controls.Add(this.radioButtonD);
-            this.groupBoxAnswers.Controls.Add(this.radioButtonC);
-            this.groupBoxAnswers.Controls.Add(this.radioButtonB);
-            this.groupBoxAnswers.Location = new System.Drawing.Point(29, 204);
+            this.groupBoxAnswers.Controls.Add(this.A);
+            this.groupBoxAnswers.Controls.Add(this.C);
+            this.groupBoxAnswers.Controls.Add(this.B);
+            this.groupBoxAnswers.Location = new System.Drawing.Point(39, 251);
+            this.groupBoxAnswers.Margin = new System.Windows.Forms.Padding(4);
             this.groupBoxAnswers.Name = "groupBoxAnswers";
-            this.groupBoxAnswers.Size = new System.Drawing.Size(596, 161);
+            this.groupBoxAnswers.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBoxAnswers.Size = new System.Drawing.Size(795, 198);
             this.groupBoxAnswers.TabIndex = 1;
             this.groupBoxAnswers.TabStop = false;
             this.groupBoxAnswers.Text = "Variante de răspuns:";
             // 
-            // labelD
-            // 
-            this.labelD.AutoSize = true;
-            this.labelD.Location = new System.Drawing.Point(45, 129);
-            this.labelD.Name = "labelD";
-            this.labelD.Size = new System.Drawing.Size(15, 13);
-            this.labelD.TabIndex = 12;
-            this.labelD.Text = "D";
-            // 
             // labelC
             // 
             this.labelC.AutoSize = true;
-            this.labelC.Location = new System.Drawing.Point(45, 98);
+            this.labelC.Location = new System.Drawing.Point(60, 121);
+            this.labelC.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelC.Name = "labelC";
-            this.labelC.Size = new System.Drawing.Size(14, 13);
+            this.labelC.Size = new System.Drawing.Size(17, 17);
             this.labelC.TabIndex = 11;
             this.labelC.Text = "C";
             // 
             // labelB
             // 
             this.labelB.AutoSize = true;
-            this.labelB.Location = new System.Drawing.Point(45, 68);
+            this.labelB.Location = new System.Drawing.Point(60, 84);
+            this.labelB.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelB.Name = "labelB";
-            this.labelB.Size = new System.Drawing.Size(14, 13);
+            this.labelB.Size = new System.Drawing.Size(17, 17);
             this.labelB.TabIndex = 10;
             this.labelB.Text = "B";
             // 
             // labelA
             // 
             this.labelA.AutoSize = true;
-            this.labelA.Location = new System.Drawing.Point(45, 38);
+            this.labelA.Location = new System.Drawing.Point(60, 47);
+            this.labelA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelA.Name = "labelA";
-            this.labelA.Size = new System.Drawing.Size(14, 13);
+            this.labelA.Size = new System.Drawing.Size(17, 17);
             this.labelA.TabIndex = 9;
             this.labelA.Text = "A";
             // 
-            // radioButtonA
+            // A
             // 
-            this.radioButtonA.AutoSize = true;
-            this.radioButtonA.Location = new System.Drawing.Point(69, 38);
-            this.radioButtonA.Name = "radioButtonA";
-            this.radioButtonA.Size = new System.Drawing.Size(14, 13);
-            this.radioButtonA.TabIndex = 5;
-            this.radioButtonA.TabStop = true;
-            this.radioButtonA.UseVisualStyleBackColor = true;
+            this.A.AutoSize = true;
+            this.A.Location = new System.Drawing.Point(92, 47);
+            this.A.Margin = new System.Windows.Forms.Padding(4);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(17, 16);
+            this.A.TabIndex = 5;
+            this.A.TabStop = true;
+            this.A.UseVisualStyleBackColor = true;
+            this.A.CheckedChanged += new System.EventHandler(this.RadioButtonA_CheckedChanged);
             // 
-            // radioButtonD
+            // C
             // 
-            this.radioButtonD.AutoSize = true;
-            this.radioButtonD.Location = new System.Drawing.Point(69, 129);
-            this.radioButtonD.Name = "radioButtonD";
-            this.radioButtonD.Size = new System.Drawing.Size(14, 13);
-            this.radioButtonD.TabIndex = 6;
-            this.radioButtonD.TabStop = true;
-            this.radioButtonD.UseVisualStyleBackColor = true;
+            this.C.AutoSize = true;
+            this.C.Location = new System.Drawing.Point(92, 121);
+            this.C.Margin = new System.Windows.Forms.Padding(4);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(17, 16);
+            this.C.TabIndex = 7;
+            this.C.TabStop = true;
+            this.C.UseVisualStyleBackColor = true;
             // 
-            // radioButtonC
+            // B
             // 
-            this.radioButtonC.AutoSize = true;
-            this.radioButtonC.Location = new System.Drawing.Point(69, 98);
-            this.radioButtonC.Name = "radioButtonC";
-            this.radioButtonC.Size = new System.Drawing.Size(14, 13);
-            this.radioButtonC.TabIndex = 7;
-            this.radioButtonC.TabStop = true;
-            this.radioButtonC.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonB
-            // 
-            this.radioButtonB.AutoSize = true;
-            this.radioButtonB.Location = new System.Drawing.Point(69, 68);
-            this.radioButtonB.Name = "radioButtonB";
-            this.radioButtonB.Size = new System.Drawing.Size(14, 13);
-            this.radioButtonB.TabIndex = 8;
-            this.radioButtonB.TabStop = true;
-            this.radioButtonB.UseVisualStyleBackColor = true;
+            this.B.AutoSize = true;
+            this.B.Location = new System.Drawing.Point(92, 84);
+            this.B.Margin = new System.Windows.Forms.Padding(4);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(17, 16);
+            this.B.TabIndex = 8;
+            this.B.TabStop = true;
+            this.B.UseVisualStyleBackColor = true;
             // 
             // panelIntro
             // 
             this.panelIntro.Controls.Add(this.textBoxTitle);
             this.panelIntro.Controls.Add(this.buttonStart);
-            this.panelIntro.Location = new System.Drawing.Point(12, 23);
+            this.panelIntro.Location = new System.Drawing.Point(19, 28);
+            this.panelIntro.Margin = new System.Windows.Forms.Padding(4);
             this.panelIntro.Name = "panelIntro";
-            this.panelIntro.Size = new System.Drawing.Size(661, 432);
+            this.panelIntro.Size = new System.Drawing.Size(881, 532);
             this.panelIntro.TabIndex = 5;
             // 
             // textBoxTitle
             // 
             this.textBoxTitle.BackColor = System.Drawing.SystemColors.Control;
             this.textBoxTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxTitle.Location = new System.Drawing.Point(97, 110);
+            this.textBoxTitle.Location = new System.Drawing.Point(129, 135);
+            this.textBoxTitle.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxTitle.Multiline = true;
             this.textBoxTitle.Name = "textBoxTitle";
-            this.textBoxTitle.Size = new System.Drawing.Size(479, 78);
+            this.textBoxTitle.Size = new System.Drawing.Size(637, 95);
             this.textBoxTitle.TabIndex = 1;
             this.textBoxTitle.Text = "Chestionar Auto\r\nCategoria B\r\n";
             this.textBoxTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -203,9 +196,10 @@
             // buttonStart
             // 
             this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.buttonStart.Location = new System.Drawing.Point(228, 272);
+            this.buttonStart.Location = new System.Drawing.Point(304, 335);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(220, 97);
+            this.buttonStart.Size = new System.Drawing.Size(293, 119);
             this.buttonStart.TabIndex = 0;
             this.buttonStart.Text = "Start";
             this.buttonStart.UseVisualStyleBackColor = true;
@@ -213,11 +207,12 @@
             // 
             // FormQuiz
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(704, 484);
+            this.ClientSize = new System.Drawing.Size(939, 596);
             this.Controls.Add(this.panelIntro);
             this.Controls.Add(this.panelQuestion);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormQuiz";
             this.Text = "Chestionar Auto";
             this.Load += new System.EventHandler(this.FormQuiz_Load);
@@ -237,15 +232,13 @@
         private System.Windows.Forms.Button buttonSubmit;
         private System.Windows.Forms.Label labelQuestions;
         private System.Windows.Forms.GroupBox groupBoxAnswers;
-        private System.Windows.Forms.RadioButton radioButtonD;
-        private System.Windows.Forms.RadioButton radioButtonC;
-        private System.Windows.Forms.RadioButton radioButtonB;
+        private System.Windows.Forms.RadioButton C;
+        private System.Windows.Forms.RadioButton B;
         private System.Windows.Forms.Panel panelIntro;
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.TextBox textBoxQuestion;
         private System.Windows.Forms.TextBox textBoxTitle;
-        private System.Windows.Forms.RadioButton radioButtonA;
-        private System.Windows.Forms.Label labelD;
+        private System.Windows.Forms.RadioButton A;
         private System.Windows.Forms.Label labelC;
         private System.Windows.Forms.Label labelB;
         private System.Windows.Forms.Label labelA;
--- DriverLicenceForm/DriverLicenceForm/FormQuiz.cs
+++ DriverLicenceForm/DriverLicenceForm/FormQuiz.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,10 @@ namespace DriverLicenceForm
 {
     public partial class FormQuiz : Form
     {
+        private QuestionsRetriever _questions = QuestionsRetriever.Instance();
+        private int _panelCount = 0;
+        private int _correctAnswers = 0;
+        private int _incorrectAnswers = 0;
         public FormQuiz()
         {
             InitializeComponent();
@@ -25,15 +30,99 @@ namespace DriverLicenceForm
         private void buttonStart_Click_1(object sender, EventArgs e)
         {
             panelQuestion.BringToFront();
+
+            textBoxQuestion.Text = _questions.QuestionsList.First().QuestionText;
+            A.Text = _questions.QuestionsList.First().AnswerTextA;
+            B.Text = _questions.QuestionsList.First().AnswerTextB;
+            C.Text = _questions.QuestionsList.First().AnswerTextC;
+
             resetAnswers();
         }
 
         private void resetAnswers()
         {
-            radioButtonA.Checked = false;
-            radioButtonB.Checked = false;
-            radioButtonC.Checked = false;
-            radioButtonD.Checked = false;
+            A.Checked = false;
+            B.Checked = false;
+            C.Checked = false;
+        }
+
+        private void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            string selectedOption = GetSelectedOption();
+
+            IncrementQuizResult(selectedOption);
+
+            if (_panelCount == _questions.QuestionsList.Count-1)
+            {
+                return;
+            }
+
+            _panelCount++;
+
+            UpdatePanel();
+
+            resetAnswers();
+        }
+
+        private void UpdatePanel()
+        {
+            try
+            {
+                textBoxQuestion.Text = _questions.QuestionsList[_panelCount].QuestionText;
+                A.Text = _questions.QuestionsList[_panelCount].AnswerTextA;
+                B.Text = _questions.QuestionsList[_panelCount].AnswerTextB;
+                C.Text = _questions.QuestionsList[_panelCount].AnswerTextC;
+            }
+            catch (System.IndexOutOfRangeException e)
+            {
+                System.Console.WriteLine(e.Message);
+                // Set IndexOutOfRangeException to the new exception's InnerException.
+                throw new System.ArgumentOutOfRangeException("index parameter is out of range.", e);
+            }
+
+        }
+
+        private void IncrementQuizResult(string selectedOption)
+        {
+            if (_questions.QuestionsList[_panelCount].CorrectAnswerText == selectedOption)
+            {
+                MessageBox.Show("Raspuns Corect!");
+                _correctAnswers++;
+            }
+            else
+            {
+                MessageBox.Show("Raspuns Incorect!");
+                _incorrectAnswers++;
+            }
+        }
+
+        private string GetSelectedOption()
+        {
+            string selectedOption = string.Empty;
+            if (A.Checked == true)
+            {
+                selectedOption = A.Name;
+            }
+            else
+            {
+                if (B.Checked == true)
+                {
+                    selectedOption = B.Name;
+                }
+                else
+                {
+                    if (C.Checked == true)
+                    {
+                        selectedOption = C.Name;
+                    }
+                }
+            }
+            return selectedOption;
+        }
+
+        private void RadioButtonA_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
